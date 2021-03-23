3f03f0caee3d47c912ff3c4fc76c441c8be78318
Oriabure Francis Abulu <80348515+abuluoria@users.noreply.github.com>
2021-03-23 12:33:54 -0400

    update
    Added description

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -3,6 +3,8 @@
 # install httpd (Linux 2 version)
 yum update -y
 yum install -y httpd.x86_64
+# start the webservice
 systemctl start httpd.service
+# enable the webservice
 systemctl enable httpd.service
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
